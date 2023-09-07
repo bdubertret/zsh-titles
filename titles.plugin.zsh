6878d7b3dc2ec18aec434975c98040762c6f04a0
Benoit Dubertret <benoit.dubertret@iscool-e.com>
2023-09-07 15:50:11 +0200

    Set prompt length to 120

--- titles.plugin.zsh
+++ titles.plugin.zsh
@@ -7,7 +7,7 @@ function update_title() {
   local a
   # escape '%' in $1, make nonprintables visible
   a=${(V)1//\%/\%\%}
-  print -nz "%20>...>$a"
+  print -nz "%120>...>$a"
   read -rz a
   # remove newlines
   a=${a//$'\n'/}
@@ -24,13 +24,13 @@ function update_title() {
 
 # called just before the prompt is printed
 function _zsh_title__precmd() {
-  update_title "zsh" "%20<...<%~"
+  update_title "zsh" "%120<...<%~"
 }
 
 # called just before a command is executed
 function _zsh_title__preexec() {
   local -a cmd
-  
+
   # Escape '\'
   1=${1//\\/\\\\\\\\}
 
@@ -41,7 +41,7 @@ function _zsh_title__preexec() {
     fg)	cmd="${(z)jobtexts[${(Q)cmd[2]:-%+}]}" ;;
     %*)	cmd="${(z)jobtexts[${(Q)cmd[1]:-%+}]}" ;;
   esac
-  update_title "$cmd" "%20<...<%~"
+  update_title "$cmd" "%120<...<%~"
 }
 
 autoload -Uz add-zsh-hook
